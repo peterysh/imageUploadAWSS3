9d2c57aef3bbaf2cc3355ef2b5468aa6a6a04634
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-18 22:47:32 +0900

    refactor: add assert between Dollar and Franc

--- toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
+++ toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
@@ -26,10 +26,13 @@ public class DollarServiceTest {
     public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals( new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals( new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
     @Test
-    public void testFrancMultiplication(){
+    public void testFrancMultiplication() {
         Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
