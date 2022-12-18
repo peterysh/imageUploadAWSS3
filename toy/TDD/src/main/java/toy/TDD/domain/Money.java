2f81a313a1173537f1374e264a5c0b78d5aefeca
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-18 22:42:30 +0900

    feat: make Money.class. amount field and equals method are moved to Money. Thanks to this. we reduce duplicate code exists in [Dollar, Franc]

---
+++ toy/TDD/src/main/java/toy/TDD/domain/Money.java
@@ -0,0 +1,11 @@
+package toy.TDD.domain;
+
+public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount;
+    }
+}
