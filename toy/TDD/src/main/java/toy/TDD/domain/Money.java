174f985f431423efa5e1e195e14d1d5772b47d89
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-19 15:51:55 +0900

    ️refactor: adjust same rules on Franc and test codes

--- toy/TDD/src/main/java/toy/TDD/domain/Money.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Money.java
@@ -9,6 +9,10 @@ public abstract class Money {
         return new Dollar(amount);
     }
 
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
+
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
