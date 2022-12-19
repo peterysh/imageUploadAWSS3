df6945592ffa77749b0b4dcdda8f5d92c01eea7f
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-19 21:27:01 +0900

    refactor: we can compile testPlusReturnsSum method

--- toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
+++ toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import toy.TDD.domain.Bank;
 import toy.TDD.domain.Expression;
 import toy.TDD.domain.Money;
+import toy.TDD.domain.Sum;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
