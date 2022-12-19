68b5b9f8ffa1b25f55258084d43c254a2c06bf34
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-19 16:09:12 +0900

    ️refactor: adjust same things in Dollar.class

--- toy/TDD/src/main/java/toy/TDD/domain/Money.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Money.java
@@ -11,7 +11,7 @@ public abstract class Money {
     };
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount){
