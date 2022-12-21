3392c054ac1eff719878f7fcdf0f6292d71596c4
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-21 21:05:17 +0900

    refactor: let's make some classes returns Expression, not Money

--- toy/TDD/src/main/java/toy/TDD/domain/Sum.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Sum.java
@@ -1,17 +1,12 @@
 package toy.TDD.domain;
 
 public class Sum implements Expression{
-    public Money augend;
-    public Money addend;
+    public Expression augend;
+    public Expression addend;
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
-    }
-
-    public Money reduce(String to) {
-        int amount = augend.amount + addend.amount;
-        return new Money(amount, to);
     }
 
     public Money reduce(Bank bank, String to){
