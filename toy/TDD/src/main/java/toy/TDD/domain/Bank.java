9475c7b0b82dae48a77b4c7296259b8f93cc920f
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-21 20:42:36 +0900

    feat: with HashTable and Pair, we can make ratio between different currency.

--- toy/TDD/src/main/java/toy/TDD/domain/Bank.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Bank.java
@@ -1,12 +1,21 @@
 package toy.TDD.domain;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     public Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
 
     public int rate(String from, String to){
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        Integer rate = (Integer)rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    public void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
