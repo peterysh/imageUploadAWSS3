d81d2170c8b434cc65334c4bef630c4f9b699cd4
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-21 21:19:01 +0900

    feat: impl plus method in Sum

--- toy/TDD/src/main/java/toy/TDD/domain/Sum.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Sum.java
@@ -16,7 +16,7 @@ public class Sum implements Expression{
     }
 
     @Override
-    public Expression plus(Expression tenFrancs) {
-        return null;
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
     }
 }
