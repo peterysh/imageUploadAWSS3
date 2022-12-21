e3be5e363b0d7e9e32954d1e2f0d8633bc2e99af
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-21 20:32:32 +0900

    refactor: make bank change currency. but I don't like a parameter named "to". I hope Bank has a kind of changing table.

--- toy/TDD/src/main/java/toy/TDD/domain/Bank.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     public Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
+
+    public int rate(String from, String to){
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
 }
