93e641505f9e1207ab4b7c5b0e720af8dae25c91
peterysh <52229539+peterysh@users.noreply.github.com>
2022-12-21 21:08:13 +0900

    feat: Expression has plus method. So we need to impl plus method in Sum. but it is stub method.

--- toy/TDD/src/main/java/toy/TDD/domain/Expression.java
+++ toy/TDD/src/main/java/toy/TDD/domain/Expression.java
@@ -2,4 +2,6 @@ package toy.TDD.domain;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+
+    Expression plus(Expression tenFrancs);
 }
--- toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
+++ toy/TDD/src/test/java/toy/TDD/dollar/DollarServiceTest.java
@@ -96,7 +96,7 @@ public class DollarServiceTest {
 
     @Test
     public void testMixedAddition(){
-        Money fiveBucks = Money.dollar(5);
+        Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
